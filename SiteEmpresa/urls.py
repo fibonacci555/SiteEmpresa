b4a96224e50f8a85e200e34323192f9650fd1b02
fibonacci555 <78218505+fibonacci555@users.noreply.github.com>
2022-09-18 00:14:00 +0000

    ddd

--- SiteEmpresa/urls.py
+++ SiteEmpresa/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from djano.conf.urls.static import static
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
